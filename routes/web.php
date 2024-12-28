d43efd0a984323079f0234f8e7d3779b5148fa10
Gabriel Santos Camargo <gabrielsantoscamargo8@gmail.com>
2024-12-28 18:53:33 -0300

    Iniciado Front com Diretivas Blade

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 // Nomeando a rota '/about'
 Route::view('/about', 'about')->name('about');
+
+Route::get('/signup', [SignupController::class, 'create']);
+
+Route::get('/login', [LoginController::class, 'create']);
